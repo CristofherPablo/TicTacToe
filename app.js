d525e4a85d439e5027e6c8945d0534f1de5f8eb9
Cristofher Pablo <chatcomchris@gmail.com>
2023-04-08 21:41:01 -0300

    Create: Function to reset the game board to its initial state

--- app.js
+++ app.js
@@ -1,24 +1,35 @@
+/* This module create and render the board */
 (function () {
   var gameBoard = {
     startCells: ['', '', '', '', '', '', '', '', ''],
+
     init: function () {
       this.cacheDom();
       this.render();
     },
+
     cacheDom: function () {
       this.$board = document.querySelector('#board');
     },
+
     render: function () {
-        var data = {
-            startCells: this.startCells,
-        };
-        data.startCells.forEach((cell) => {
-        let cellElement = document.createElement('div');
+      var data = {
+        startCells: this.startCells,
+      };
+      data.startCells.forEach((cell, index) => {
+        let cellElement = document.createElement('button');
         cellElement.classList.add('cell');
+        cellElement.setAttribute('data-index', index);
         this.$board.appendChild(cellElement);
       });
+    },
+
+    reset: function(){
+        this.$board.innerHTML = '';
+        this.render();
     },
   };
 
   gameBoard.init();
+  console.log(gameBoard.startCells);
 })();
