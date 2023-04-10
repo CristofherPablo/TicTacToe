b3d19f2a3e53514fdfffddc9861cd0970de910fb
Cristofher Pablo <chatcomchris@gmail.com>
2023-04-10 18:45:29 -0300

    Fix: Problem with the O symbel and add fucntion to switch players

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 /* This module create and render the board */
 (function () {
+  var player = {
+    playerOne: 1,
+    playerTwo: 0,
+  };
+
   var gameBoard = {
     startCells: ['', '', '', '', '', '', '', '', ''],
 
@@ -20,8 +25,8 @@
         let cellElement = document.createElement('button');
         cellElement.classList.add('cell');
         cellElement.setAttribute('data-index', index);
-        let div = document.createElement('div');
-        cellElement.appendChild(div);
+        let img = document.createElement('img');
+        cellElement.appendChild(img);
         this.$board.appendChild(cellElement);
       });
     },
@@ -48,11 +53,25 @@
       });
     },
 
-    render: function () {},
+    switchPlayers: function (player) {
+      if (player.playerOne === 0) {
+        player.playerOne = 1;
+        player.playerTwo = 0;
+      } else {
+        player.playerOne = 0;
+        player.playerTwo = 1;
+      }
+    },
 
     addPlayerSymbol: function (event) {
-      let $div = event.target.querySelector('div');
-      $div.classList.add('circle');
+      let $img = event.target.querySelector('img');
+      if (player.playerOne === 0) {
+        $img.src = 'assets/cross.png';
+        this.switchPlayers(player);
+      } else {
+        $img.src = 'assets/circle.png';
+        this.switchPlayers(player);
+      }
     },
   };
 
